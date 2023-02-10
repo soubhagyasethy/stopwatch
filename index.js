4923c845cd64555103d7c1f7d8ee1b295fbf6cc3
soubhagyasethy <ssethy017@gmail.com>
2023-02-10 14:03:00 +0530

    added comments

--- index.js
+++ index.js
@@ -16,14 +16,14 @@ function stopTimer() {
 }
 
 // run this function if reset button clicked
-function resetTimer() {
+function resetTimer() {   
     timer = false;
 
     tens = 0;
     sec = 0;
     min = 0;
 
-    document.getElementById("tens").innerHTML = "00";
+    document.getElementById("tens").innerHTML = "00"; /* to display in double digits when timer get reset */
     document.getElementById("sec").innerHTML = "00";
     document.getElementById("min").innerHTML = "00";
 }
@@ -42,8 +42,8 @@ function stopWatch() {
                 sec = 0;
             }
         }
-        
-        let tensString = tens;
+      
+        let tensString = tens;   /* to display in double digits when tens, sec & min are less than 10 */
         let secString = sec;
         let minString = min;
 
@@ -61,6 +61,6 @@ function stopWatch() {
         document.getElementById("sec").innerHTML = secString;
         document.getElementById("tens").innerHTML = tensString;
 
-        setTimeout("stopWatch()", 10)
+        setTimeout("stopWatch()", 10) /* run the function stopwatch in after every 10 milli seconds */
     }
 }
