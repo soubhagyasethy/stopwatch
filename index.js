8e15e40800fb19a3ed9e05a39c107ecb1b69c3f5
soubhagyasethy <ssethy017@gmail.com>
2023-03-08 20:20:07 +0530

    fix: fixed the increasing speed of the timer when clicked on start repeatedly

--- index.js
+++ index.js
@@ -2,65 +2,50 @@ let tens = 0;
 let sec = 0;
 let min = 0;
 
-let timer = false;
+let timer = null;
 
-// run this function if start button clicked
+// for start
 function startTimer() {
-    timer = true;
-    stopWatch()
+  /*clearing the interval here will not trigger the function repeatedly if we press start button again & again */
+  if (timer !== null) {
+    clearInterval(timer);
+  }
+  timer = setInterval(stopWatch, 10);
 }
 
-// run this function if stop button clicked
+// for stop
 function stopTimer() {
-    timer = false;
+  clearInterval(timer);
 }
 
-// run this function if reset button clicked
-function resetTimer() {   
-    timer = false;
-
-    tens = 0;
-    sec = 0;
-    min = 0;
-
-    document.getElementById("tens").innerHTML = "00"; /* to display in double digits when timer get reset */
-    document.getElementById("sec").innerHTML = "00";
-    document.getElementById("min").innerHTML = "00";
+// for reset
+function resetTimer() {
+  clearInterval(timer);
+  tens = 0;
+  sec = 0;
+  min = 0;
+  /* to display in double digits when timer get reset */
+  document.getElementById("tens").innerHTML = "00";
+  document.getElementById("sec").innerHTML = "00";
+  document.getElementById("min").innerHTML = "00";
 }
 
-// 
 function stopWatch() {
-    if (timer == true) {
-        tens = tens + 1;
-        
-        if(tens == 99) {
-            sec = sec + 1;
-            tens = 0;
-
-            if(sec == 59) {
-                min = min + 1;
-                sec = 0;
-            }
-        }
-      
-        let tensString = tens;   /* to display in double digits when tens, sec & min are less than 10 */
-        let secString = sec;
-        let minString = min;
-
-        if(tens < 10) {
-            tensString = "0" + tensString;
-        }
-        if(sec < 10) {
-            secString = "0" + secString;
-        }
-        if(min < 10) {
-            minString = "0" + minString;
-        } 
-
-        document.getElementById("min").innerHTML = minString;
-        document.getElementById("sec").innerHTML = secString;
-        document.getElementById("tens").innerHTML = tensString;
-
-        setTimeout("stopWatch()", 10) /* run the function stopwatch in after every 10 milli seconds */
+  tens++;
+  if (tens == 99) {
+    tens = 0;
+    sec++;
+    if (sec == 59) {
+      sec = 0;
+      min++;
     }
+  }
+  /* to display in double digits when tens, sec & min are less than 10 */
+  let tensString = tens < 10 ? "0" + tens : tens;
+  let secString = sec < 10 ? "0" + sec : sec;
+  let minString = min < 10 ? "0" + min : min;
+
+  document.getElementById("min").innerHTML = minString;
+  document.getElementById("sec").innerHTML = secString;
+  document.getElementById("tens").innerHTML = tensString;
 }
